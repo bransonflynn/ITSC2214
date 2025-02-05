d2f03e96d88416c6cb815e2845915015cdd06e22
bp42s <140995694+bp42s@users.noreply.github.com>
2025-02-04 23:54:38 -0500

    Project2: Added more javadoc and tidied up code a bit

--- Project2/src/App2.java
+++ Project2/src/App2.java
@@ -1,6 +1,5 @@
 public class App2 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println("App2.java -> Started!");
     }
 }
--- Project2/submit/WordFrequency.java
+++ Project2/submit/WordFrequency.java
@@ -2,7 +2,7 @@ package Project2.submit;
 
 /**
  * Custom object to store "words" as string:int key:value pair for use in
- * HashWords.java
+ * HashWords
  * 
  * @author Branson Flynn
  * @version Feb 04, 2025
@@ -13,20 +13,47 @@ public class WordFrequency {
     private String word;
     private int count;
 
+    /**
+     * Class constructor.
+     * 
+     * @param w The text that this word contains.
+     */
     public WordFrequency(String w) {
         this.word = w;
         this.count = 1;
     }
 
+    /**
+     * Returns the text that this word contains.
+     * 
+     * @return The text that this word contains.
+     */
     public String getWord() {
         return this.word;
     }
 
+    /**
+     * Returns the amount of times this word has appeared in the containing table.
+     * 
+     * @return The amount of times this word has appeared in the containing table.
+     */
     public int getCount() {
         return this.count;
     }
 
+    /**
+     * Increases the count of this word by 1.
+     */
     public void increment() {
         this.count++;
+    }
+
+    /**
+     * Decreases the count of this word by 1.
+     */
+    public void decrement() {
+        this.count--;
+        if (this.count < 0)
+            this.count = 0;
     }
 }
